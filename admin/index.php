c0b9c33a17325c3b720e2bcbdaae42afe3a658d5
Dmitri Sychev <melnikmi@pik.ru>
2024-01-06 15:48:42 +0600

    add authorization for admin panel

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<body>
+
+  <?php
+    ?>
+  <main>
+    <div class="quotes-block">
+     Admin panel
+    </div>
+  </main>
+  <?php
+  echo dirname(__FILE__);
+    ?>
+</body>
+
+</html>
